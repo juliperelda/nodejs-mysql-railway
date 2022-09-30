e1b71ba6de3f8cf6eef00aaab8140d45d058f8c4
Julian Perelda <106119182+juliperelda@users.noreply.github.com>
2022-09-30 08:29:14 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,13 @@
 export const PORT = process.env.PORT || 3000
 
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_USER = process.env.DB_USERT || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || '40928607'
-export const DB_NAME = process.env.DB_NAME || 'usersdb'
-export const DB_PORT = process.env.DB_PORT || 3306
+// export const DB_HOST = process.env.DB_HOST || 'localhost'
+// export const DB_USER = process.env.DB_USERT || 'root'
+// export const DB_PASSWORD = process.env.DB_PASSWORD || '40928607'
+// export const DB_NAME = process.env.DB_NAME || 'usersdb'
+// export const DB_PORT = process.env.DB_PORT || 3306
+
+export const DB_HOST = 'containers-us-west-37.railway.app'
+export const DB_USER = 'root'
+export const DB_PASSWORD = 'qVSY4hqnLmwfDwq7D6EY'
+export const DB_NAME = 'railway'
+export const DB_PORT = 7306
